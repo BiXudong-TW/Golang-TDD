888eb8212ba06b3baf61befe5c3406b990dd7d51
Xudong Bi <xudong.bi@thoughtworks.com>
2023-01-16 22:35:35 +0800

    feat: add array test

--- array/array_test.go
+++ array/array_test.go
@@ -4,11 +4,32 @@ import (
 	"testing"
 )
 
-func Test_Claim(t *testing.T) {
+func Test_should_init_array_correctly1(t *testing.T) {
 	actual := [3]int{1, 2}
-	//Please fill all the array elements of the actual
-	expect := [3]int{1, 2, 0}
-	if actual != expect {
+	//Please fill all the elements of the expect array
+	expect := [3]int{}
+
+	if expect != actual {
+		t.Fail()
+	}
+}
+
+func Test_should_init_array_correctly2(t *testing.T) {
+	actual := [5]int{2: 4, 4: 2}
+	//Please fill all the elements of the expect array
+	expect := [5]int{}
+
+	if expect != actual {
+		t.Fail()
+	}
+}
+
+func Test_should_get_correct_array_length(t *testing.T) {
+	actual := [...]int{1, 2, 3}
+	//Please update the expect value correctly
+	expect := 0
+
+	if expect != len(actual) {
 		t.Fail()
 	}
 }
